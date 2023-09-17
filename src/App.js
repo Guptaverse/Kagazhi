594fc2f1d90621b27c616d3647559ae89b91bd69
Guptaverse <sggreat88@gmail.com>
2023-09-17 22:56:52 +0530

    delete functionality added

--- src/App.js
+++ src/App.js
@@ -1,59 +1,83 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [inputTitle, setTitle] = useState('');
-  const [inputText, setInputText] = useState('');
+  const [inputTitle, setTitle] = useState("");
+  const [inputText, setInputText] = useState("");
   const [divs, setDivs] = useState([]);
 
+  const deleteDiv = async (title) => {
+    await axios
+      .delete(`${process.env.REACT_APP_Backend_URL}delete/${title}`)
+      .then((res) => {
+        if (res.status === 204) {
+          console.log("deleted from backend");
+          createDivhandler();
+        } else {
+          // Handle error responses from the server
+          console.log("some error");
+        }
+      })
+      .catch((err) => {
+        // Handle network errors or other exceptions
+        console.log(err);
+      });
+  };
+  const createDivhandler = async () => {
+    console.log("I am inside createHandler");
+    try {
+      const newDivs = [];
+      await axios
+        .get(`${process.env.REACT_APP_Backend_URL}data`)
+        .then((res) => {
+          const response = res.data.data;
+          response.map((item, idx) => {
+            const { title, description } = item;
 
-  const handleData = async()=>{
-    await axios.get(`${process.env.REACT_APP_Backend_URL}data`)
-    .then((res)=>{
-      if (Array.isArray(res.data)) {
-        res.data.map((item) => {
-          console.log(item.title);
-           // Make sure to return the mapped item if needed
-        })}
-    })
-    .catch((err)=>{
-      console.error("Error",err)
-    })
-  }
-  const createDiv = async() => {
-    const title = inputTitle + "" +divs.length;
-    const data = {title,inputText}
-    if (inputText && inputTitle) {
-      // Create a new div element
-      const newDiv = (
-        <div key={divs.length} className="created-div">
-          <h3>{`${inputTitle} #${1+divs.length}`}</h3>
-          {inputText}
-        </div>
-      );
+            console.log(title, " ", description);
 
-      // Update the divs array with the new div
-      setDivs([newDiv,...divs]);
+            const newDiv = (
+              <div key={idx} className="created-div">
+                <h3>{title}</h3>
+                {description}
+                <button onClick={() => deleteDiv(title)}>delete</button>
+              </div>
+            );
+            newDivs.push(newDiv);
 
-      // Clear the input field
-      setInputText('');
-
+            console.log();
+          });
+          setDivs(newDivs);
+          setTitle("");
+          setInputText("");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } catch (error) {
+      console.error("Error fetching data:", error);
     }
-    await axios.post(process.env.REACT_APP_Backend_URL,data)
-    .then((res)=>{
-      console.log(res.data)
-    })
-    .catch((err)=>{
-      console.error("Error",err)
-    })
+    console.log("I am inside createHandler");
+  };
+  const createDiv = async () => {
+    const title = inputTitle + "" + divs.length;
+    const data = { title, inputText };
 
-    
+    await axios
+      .post(process.env.REACT_APP_Backend_URL, data)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.error("Error", err);
+      });
+    createDivhandler();
   };
 
   return (
     <div className="App">
-      <h1>Dynamic Div Creator (React)</h1>
+      <h1>Kagazhi : Note taking app</h1>
       <input
         type="text"
         placeholder="Enter title"
@@ -66,8 +90,8 @@ function App() {
         value={inputText}
         onChange={(e) => setInputText(e.target.value)}
       />
-      <button onClick={createDiv}>Create Div</button>
-      <button onClick={handleData}>Data</button>
+      <button onClick={createDiv}>Save</button>
+      {/* <button onClick={handleData}>Data</button> */}
       <div className="container">{divs}</div>
     </div>
   );
